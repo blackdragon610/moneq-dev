f9f7ff2d9c7315615013cd9e311e4caab85421f9
Anatoliy Murusev <tolik.murusev.02@mail.ru>
2020-11-17 03:28:41 +0900

    fixed to add bootstrap to sass

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,6 @@
 
 @section('main')
 
-ddd
+
 
 @endsection
---
+++ resources/views/layouts/parts/header.blade.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="http://placehold.it/150x50?text=Logo" alt="">
+    </a>
+</nav>
