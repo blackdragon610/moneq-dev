dfe58d2c5dd45551ee0cbfc845caa1dab376ab42
Anatoliy Murusev <tolik.murusev.02@mail.ru>
2020-11-18 03:41:59 +0900

    made top page

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -11,6 +11,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<link rel="stylesheet" href="css/app.css">
 		<link rel="stylesheet" href="css/test.css">
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         <title></title>
 
         @if (env("APP_DEBUG"))
