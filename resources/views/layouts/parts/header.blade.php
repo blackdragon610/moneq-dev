cf2989d436c2cd23cfb35b60eff0d52db4dfe199
Anatoliy Murusev <tolik.murusev.02@mail.ru>
2020-11-18 23:51:55 +0900

    header for logged in user

--- resources/views/layouts/parts/header.blade.php
+++ resources/views/layouts/parts/header.blade.php
@@ -1,15 +1,45 @@
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-expand-sm navbar-light bg-light">
     <a class="navbar-brand" href="#">
         <img src="http://placehold.it/70x70?text=Savee" alt="">
-    </a>
+    </a>    
 
-    <div class="nav navbar-nav navbar-right">
+    <!-- AFTER LOGIN -->
+    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-3" aria-controls="navbarSupportedContent-3" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent-3">
+        <ul class="navbar-nav ml-auto nav-flex-icons">
+            <li class="nav-item dropdown">
+                <a class="nav-link waves-effect waves-light" id="navbarDropdownBell" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fa fa-bell-o fa-lg has-badge" data-count="4"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-default" aria-labelledby="navbarDropdownBell">
+                    <a class="dropdown-item waves-effect waves-light" href="#">Another action</a>
+                    <a class="dropdown-item waves-effect waves-light" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link waves-effect waves-light" id="navbarDropdownUser" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fa fa-user-o fa-lg"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-default" aria-labelledby="navbarDropdownUser">
+                    <a class="dropdown-item waves-effect waves-light" href="#">Action</a>
+                    <a class="dropdown-item waves-effect waves-light" href="#">Another action</a>
+                    <a class="dropdown-item waves-effect waves-light" href="#">Something else here</a>
+                </div>
+            </li>
+        </ul>
+    </div> -->
+
+    <!-- Before LOGIN  -->
+    <div class="nav navbar-nav navbar-right ml-auto">
         <div class="btn-toolbar">
             <a href="{{ url('/register') }}" class="btn btn-outline-orange mx-2">@lang('string.register')</a>
             <a href="{{ url('/login') }}" class="btn btn-outline-red">@lang('string.login')</a>
         </div>   
      </div>
 </nav>
+    
 <div class="container-fluid yellowpanel">
     <div class="row">
         <div class="container-fluid">
