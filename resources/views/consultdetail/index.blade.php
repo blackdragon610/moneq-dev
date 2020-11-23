0c9ac3a3f47729ba5890c1aa2c33ac5db24cf34d
Anatoliy Murusev <tolik.murusev.02@mail.ru>
2020-11-23 18:28:38 +0900

    consult detail index page's left pane

--- resources/views/consultdetail/index.blade.php
+++ resources/views/consultdetail/index.blade.php
@@ -20,47 +20,61 @@
         </div>
         
         <div class="col-12 col-sm-9 order-sm-1 order-2" id="main">
-            <div class="lightgreypanel">
+            <div class="container">
                 <section>
-                    <div class="container-fluid whitepanel">
-                        @include('layouts.parts.custom.articledetail', ["type" => "articledetail", 'name' => 'article', 'contents' => ''])
+                    <div class="row">
+                        <div class="container-fluid whitepanel">
+                            @include('layouts.parts.custom.articledetail', ["type" => "articledetail", 'name' => 'article', 'contents' => ''])
+                        </div>
                     </div>
                 </section>
             </div>
 
-            <div class="lightgreypanel">
+            <div class="container">
                 <section>
-                    <div class="container-fluid whitepanel">
-                        <h5 class="font-weight-bold p-2">2名の専門科が回答しています</h5>
-                        <hr class="mt-2 mb-3"/>
-                        @include('layouts.parts.custom.answer', ["type" => "answer", 'name' => 'answer', 'contents' => ''])
-                        @include('layouts.parts.custom.answer', ["type" => "answer", 'name' => 'answer', 'contents' => ''])
+                    <div class="row">
+                        <div class="container-fluid whitepanel">
+                            <h5 class="font-weight-bold p-2">2名の専門科が回答しています</h5>
+                            <hr class="mt-2 mb-3"/>
+                            @include('layouts.parts.custom.answer', ["type" => "answer", 'name' => 'answer', 'contents' => ''])
+                            @include('layouts.parts.custom.answer', ["type" => "answer", 'name' => 'answer', 'contents' => ''])
+                        </div>
                     </div>
-
+                    <div class="row">
+                        <div class="container-fluid whitepanel pb-3 pt-3">
+                            <div class="container border border-dark pb-3">
+                                <h5 class="font-weight-bold text-center p-2">有料会員になるとお金の専門家に相談できます</h5>
+                                <hr class="mt-2 mb-3"/>
+                                <ul>
+                                    <li><i class="fa fa-check"></i>月300円（税別）</li>
+                                    <li><i class="fa fa-check"></i>毎月3回まで質問が可能</li>
+                                    <li><i class="fa fa-check"></i>最短5分で回答可能</li>
+                                    <li><i class="fa fa-check"></i>100名超の認定専門家が回答</li>
+                                    <li><i class="fa fa-check"></i>回答率99％</li>
+                                </ul>
+                                <div class="col text-center ">
+                                    <button class="btnSelected">専門家に相談をする（有料会員）</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </div>
 
-            <div class="lightgreypanel">
-                <div class="container-fluid whitepanel">
-                    <h5 class="font-weight-bold text-center p-2">有料会員になるとお金の専門家に相談できます</h5>
-                    <hr class="mt-2 mb-3"/>
-                    <ul>
-                        <li><i class="fa fa-check"></i>月300円（税別）</li>
-                        <li><i class="fa fa-check"></i>毎月3回まで質問が可能</li>
-                        <li><i class="fa fa-check"></i>最短5分で回答可能</li>
-                        <li><i class="fa fa-check"></i>100名超の認定専門家が回答</li>
-                        <li><i class="fa fa-check"></i>回答率99％</li>
-                    </ul>
-                    <div class="col text-center ">
-                        <button class="btnSelected">専門家に相談をする（有料会員）</button>
+            <div class="container">
+                <section>
+                    <div class="row">
+                        <div class="container-fluid whitepanel pb-3">
+                            <h5 class="font-weight-bold p-2">関連する質問</h5>
+                            <hr class="mt-2 mb-3"/>
+                            @include('layouts.parts.custom.question', ["type" => "question", 'name' => 'question', 'contents' => ''])
+                            @include('layouts.parts.custom.question', ["type" => "question", 'name' => 'question', 'contents' => ''])
+                        </div>
                     </div>
-                </div>
+                </section>
             </div>
-
 
         </div>
-
-
     </div>
 </div>
 
---
+++ resources/views/layouts/parts/custom/question.blade.php
@@ -0,0 +1,12 @@
+<article class="col-12 p-1 bg-white">
+    <div class="row pl-3 pt-2">
+        <h5 class="font-weight-bold">生命保険に関して教えてください</h5>
+        <img src="/images/insurance-icon.png" alt="">
+    </div>
+    <div class="row pl-3">
+        <span class="name">テスト太郎さん</span>
+        <p class="keepOneLine">入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良い</p>
+        <span class="name">3名が回答</span>
+        <span class="age ml-auto">2020/10/29</span>
+    </div>
+</article>
