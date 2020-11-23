106a79fced184e110903359ec2aa07beb4942c55
Anatoliy Murusev <tolik.murusev.02@mail.ru>
2020-11-23 10:33:39 +0900

    consult dtail

--- resources/views/layouts/parts/custom/answer.blade.php
+++ resources/views/layouts/parts/custom/answer.blade.php
@@ -1,37 +1,32 @@
-<article class="col-12 p-1">
+<article class="col-12 p-1 bg-white">
     <div class="row">
-        <div class="col-sm-2 col-lg-1 pt-2">
+        <div class="col-sm-2 pt-2">
             <img src="http://placehold.it/50x50?text=P" alt="">
         </div>
-        <div class="col-sm-10 col-lg-4 pt-2">
+        <div class="col-sm-7 pt-2">
             <div class="container-fluid">
                 <div class="row">
                     <h5 class="font-weight-bold">テスト花子さん</h5>
-                    <p>（テストハナコ）</p>
                 </div>
                 <div class="row">
-                    <span class="age">30代前半／女性</span>
-                    <span class="address">東京都</span>
+                    <img src="/images/insurance-icon.png" alt="">
                 </div>
             </div>
         </div>
 
-        <div class="col-sm-4 col-lg-3 pt-2">
-            <div class="container-fluid">
-                <div class="row pb-lg-1">
-                    <h6 class="mright font-weight-bold">回答数</h6>
-                    <span>125件</span>
-                </div>
-                <div class="row">
-                    <span class="mright font-weight-bold">3役に立った</span>
-                    <span >50件</span>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-8 col-lg-4 pt-2">
+        <div class="col-sm-3 pt-2">
             <div class="container-fluid pl-0 pb-lg-1">
-                <h6 class="font-weight-bold">保有資格</h6>
-                <span class="mright keepTwoLine">1級ファイナンシャル・プランニング技能士</span>
+                <span class="mright keepTwoLine">2020/10/26</span>
             </div>
         </div>
+    </div>
+    <div class="container">
+        <p>入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良いです。入った方が良い</p>
+        <p>
+            <a href="" class="text-dark">役に立った</a>
+        </p>
+    </div>
+    <div class="col text-center ">
+        <button class="btnSubmit">この専門家の回答で解決</button>
+    </div>
 </article>
