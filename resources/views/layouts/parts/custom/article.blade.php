3f1788103a1a16d1c9148b7ea1a06cc72819371c
Anatoliy Murusev <tolik.murusev.02@mail.ru>
2020-11-25 04:20:34 +0900

    XD top page - article fixed

--- resources/views/layouts/parts/custom/article.blade.php
+++ resources/views/layouts/parts/custom/article.blade.php
@@ -8,10 +8,16 @@
             </div>
         </div>
         <div id="title">保険のことで質問です</div>
-        <p id="description" class="keepTwoLine">主人の生命保険のことで相談があります。がん保険は入ったほうがよろしいでしょうか？主人の生命保険のことで相談があります。がん保険は入ったほうがよろしいでしょうか？主人の生命保険のことで相談があります。がん保険は入ったほうがよろしいでしょ...</p>
-        <img src="/images/img-avatar-sample.png" class="rounded_circle" id="avatar">
+        <p id="description" class="keepTwoLine mb-0">主人の生命保険のことで相談があります。がん保険は入ったほうがよろしいでしょうか？主人の生命保険のことで相談があります。がん保険は入ったほうがよろしいでしょうか？主人の生命保険のことで相談があります。がん保険は入ったほうがよろしいでしょ...</p>
+        <img src="/images/img-avatar-sample.png" class="avatar" id="avatar">
         <span id="name">テスト太郎さん</span>
         <span id="age">30代／男性</span>
-        <span id="answers"><img src="/images/svg/img-checkbox-red-checked.svg">未解決</span>
+        <span id="solved"><img src="/images/svg/img-checkbox-red-checked.svg">未解決</span>
+        <img src="/images/svg/img-dashline.svg" style="margin-top:10px;height:1px">
+        <div class="row m-0 align-items-center" style="padding-top:10px">
+            <img src="/images/svg/img-avatar-grandfa.svg" class="avatar">
+            <img src="/images/svg/img-avatar-young.svg" class="avatar" style="margin-left:10px">
+            <span id="persons">2名</span><span id="answers">が回答</span>
+        </div>
     </div>
 </article>
