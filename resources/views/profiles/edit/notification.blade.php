00c81f942faa166d04c42db224425145b262b503
Anatoliy Murusev <tolik.murusev.02@mail.ru>
2020-12-01 06:31:25 +0900

    fixed ui notification

--- resources/views/profiles/edit/notification.blade.php
+++ resources/views/profiles/edit/notification.blade.php
@@ -1,46 +1,53 @@
 @extends('layouts/front', ["type" => 1])
 
 @section('main')
-<div class="lightgreypanel">
-    <div class="container p-3">
+<div class="whitepanel">
+    <div class="container">
 
-        <section>
-            <div class="row">
-                <div class="col-md-12 col-lg-12 bg-white">
-                    <h5 class="font-weight-bold p-2">通知設定</h5>
-                    <hr class="mt-2 mb-3"/>
-                       {{Form::open(['url'=> route('profiles.notification.update'),'method'=>'POST', 'files' => false, 'id' => 'form'])}}
-                         <div class="container row p-2">
-                            <div class="col">
-                                <label class="checkbox-inline">
-                                    <input type="checkbox" <?php if($user->is_send_answer == 1) echo 'checked'?> data-toggle="toggle" data-style="ios" name="is_send_answer"> 回答通知
-                                </label>
-                           </div>
-                            <div class="col">
-                                <label class="checkbox-inline">
-                                    <input type="checkbox" <?php if($user->is_send_message == 1) echo 'checked'?> data-toggle="toggle" data-style="ios" name="is_send_message"> メッセージの通知
-                                </label>
-                            </div>
-                            <div class="col">
-                                <label class="checkbox-inline">
-                                    <input type="checkbox" <?php if($user->is_send_master == 1) echo 'checked'?> data-toggle="toggle" data-style="ios" name=is_send_master> MoneQの通知
-                                </label>
-                            </div>
+        <div class="row" style="margin-bottom:80px">
+            <div class="col-md-12 col-lg-12 bg-white">
+                <p class="title-medium" style="padding-left:70px">通知設定</h5>
+                <!-- <hr class="mt-2 mb-3"/> -->
+
+                {{Form::open(['url'=> route('profiles.notification.update'),'method'=>'POST', 'files' => false, 'id' => 'form'])}}
+                <div class="container" style="padding-left:70px;padding-right:70px">
+                    <div class="col-7">
+                        <div class="row">
+                            <label class="cl-switch cl-switch-red">
+                                <div style="display:inline-block;width:180px"><span class="label">回答通知</span></div>
+                                <input type="checkbox" <?php if(/*$user->is_send_answer == 1*/true) echo 'checked'?> name="is_send_answer"/>
+                                <span class="switcher"></span>
+                            </label>
                         </div>
-                        <section>
-                            <div class="d-flex justify-content-end">
-                                <button class="btnSubmit">次へ</button>
-                            </div>
-                        </section>
-                        {{Form::close()}}
-                        {{Form::open(['url'=> route('profiles.manage'),'method'=>'GET', 'files' => false, 'id' => 'form'])}}
-                            <section style="position:absolute; bottom:0px;">
-                                <button class="btnUnselected">入力をスキップする</button>
-                            </section>
-                        {{Form::close()}}
+                        <div class="row">
+                            <label class="cl-switch cl-switch-red">
+                                <div style="display:inline-block;width:180px"><span class="label">メッセージの通知</span></div>
+                                <input type="checkbox" <?php if(/*$user->is_send_message == 1*/true) echo 'checked'?> name="is_send_message"/>
+                                <span class="switcher"></span>
+                            </label>
+
+                        </div>
+                        <div class="row">
+                            <label class="cl-switch cl-switch-red">
+                                <div style="display:inline-block;width:180px"><span class="label">MoneQの通知</span></div>
+                                <input type="checkbox" <?php if(/*$user->is_send_master == 1*/false) echo 'checked'?> name="is_send_master"/>
+                                <span class="switcher"></span>
+                            </label>
+                        </div>                        
+                    </div>
+                    <div class="col-5" style="width:300px">
+                        <div class="row">
+                            <button class="btn yellow-btn-304-50">変更を送信</button>
+                        </div>                        
+                        <div class="row" style="margin-top:12px">
+                            <button class="btnUnselected" style="width:300px">会員情報に戻る</button>
+                        </div>                        
+                    </div>
                 </div>
             </div>
-        </section>
+            {{Form::close()}}
+
+        </div>
 
     </div>
 </div>
