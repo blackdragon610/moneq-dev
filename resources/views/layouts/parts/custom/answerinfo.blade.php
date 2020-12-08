ff49cc8c18c4125efbfdcd584da595537c641b18
Anatoliy Murusev <tolik.murusev.02@mail.ru>
2020-12-09 02:16:22 +0900

    added ranking crown to answerinfo

--- resources/views/layouts/parts/custom/answerinfo.blade.php
+++ resources/views/layouts/parts/custom/answerinfo.blade.php
@@ -1,13 +1,19 @@
-<div class="container-fluid bg-white" style="border:1px solid #dbdbdb; padding: 10px;">
+<div class="col-12">
+    <div class="row" style="padding-left:15px">
+        <img src="/images/svg/img-left-ranking-1.svg" style="position:absolute">
 
-    <div class="col-3 p-0">
-        <a href="{{route('expert.detail', $contents->id)}}">
-            <img src="/images/img-avatar-sample.png">
-        </a>
-    </div>
-    <div class="col-9 pt-2">
-            <p class="title-14px">{{$contents->expert_name_second}}</p>
-            <span class="label-12px" style="color:#9B9B9B">回答数 : {{$contents->amount}} </span>
-    </div>
+        <div class="container-fluid bg-white" style="border:1px solid #dbdbdb; padding: 10px;">
+
+            <div class="col-3 p-0">
+                <a href="{{route('expert.detail', $contents->id)}}">
+                    <img src="/images/img-avatar-sample.png">
+                </a>
+            </div>
+            <div class="col-9 pt-2">
+                    <p class="title-14px">{{$contents->expert_name_second}}</p>
+                    <span class="label-12px" style="color:#9B9B9B">回答数 : {{$contents->amount}} </span>
+            </div>
+        </div>
+
 </div>
-
+</article>
